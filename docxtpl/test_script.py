908501a651ed99993fa25059d6aaedc01a28b0d8
Brady Lamprecht <bdlamprecht@gmail.com>
2017-02-06 17:40:41 -0700

    Working docx generator

---
+++ docxtpl/test_script.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import time
+import os
+import pwd
+from getpass import getuser
+from docxtpl import DocxTemplate
+
+doc = DocxTemplate("test_env.docx.j2")
+output = { 'test_name' : 'Test Envrionment',
+           'tester' : "{} ({})".format(pwd.getpwuid(os.getuid())[4],
+                                     pwd.getpwuid(os.getuid())[0]),
+           'date' : time.strftime("%d%b%Y"),
+
+
+           'test1_result' : 'show inventory',
+           'test1_comment': 'inventory results',
+           'test2_result' : 'show envrionment',
+           'test2_comment': 'envrionment results',
+           'test3_result' : 'show version',
+           'test3_comment': 'version results',
+           'test4_result' : 'show lldp neighbors',
+           'test4_comment': 'lldp results',
+           'test5_result' : 'show run',
+           'test5_comment': 'run results',
+           'test6_result' : 'show clock',
+           'test6_comment': 'clock results',
+           'test7_result' : 'show users',
+           'test7_comment': 'user results',
+           'test8_result' : 'show boot',
+           'test8_comment': 'boot results',
+           'test9_result' : 'show interface status',
+           'test9_comment': 'interface results'}
+
+doc.render(output)
+doc.save('generated.docx')
