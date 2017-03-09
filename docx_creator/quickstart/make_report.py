348e3a8ba76a32d76a44efe3f98479b08945eb68
Brady Lamprecht <bdlamprecht@gmail.com>
2017-03-09 15:49:38 -0700

    Fixed table formatting

--- docx_creator/quickstart/make_report.py
+++ docx_creator/quickstart/make_report.py
@@ -4,6 +4,11 @@ from docx import Document
 from docx.shared import Pt
 from docx.enum.style import WD_STYLE_TYPE
 
+def delete_paragraph(paragraph):
+  p = paragraph._element
+  p.getparent().remove(p)
+  p._p = p.element = None
+
 doc = Document('input.docx')
 
 styles = doc.styles
@@ -31,6 +36,9 @@ print('\nTable Styles:')
 for style in table_styles:
   print('Style Name: \t{}'.format(style.name))
 
+j2var_open  =  '{{ '
+j2var_close = ' }}'
+
 for i in range(1,3):
   doc.add_paragraph('Test {}'.format(i), style='test_header')
   doc.add_paragraph('Description', style='desc_header')
@@ -43,30 +51,13 @@ for i in range(1,3):
   doc.add_paragraph('[junos command here]', style='output')
 
   doc.add_paragraph('Test Output', style='desc_header')
-
-  # This section is to get the font size in the following table to be correct size.
-  outp = doc.add_paragraph('This is some text')
-  outp = doc.paragraphs[-1]
-  outp.style.font.size = Pt(8)
-  p = outp._element
-  p.getparent().remove(p)
-  p._p = p._element = None
-
+  result = j2var_open + 'test{}_result'.format(i) + j2var_close
   o_table = doc.add_table(1,1,style='test_output')
-  o_table.cell(0,0).text = '{{ test{}_results }}'.format(i)
-  #o_table.cell(0,0).add_paragraph('{{ test{}_results }}'.format(i),style='output_sm')
-
-  # This section is to get the font size in the following table to be correct size.
-  comm = doc.add_paragraph('This is some text')
-  comm = doc.paragraphs[-1]
-  comm.style.font.size = Pt(10)
-  p = comm._element
-  p.getparent().remove(p)
-  p._p = p._element = None
+  o_table.cell(0,0).add_paragraph(result,style='output_sm')
 
   doc.add_paragraph('Test Comments', style='desc_header')
+  comment = j2var_open + 'test{}_comment'.format(i) + j2var_close
   c_table = doc.add_table(1,1,style='test_comment')
-  c_table.cell(0,0).text = '{{ test{}_comment }}'.format(i)
-  #c_table.cell(0,0).add_paragraph('{{ test{}_comment }}'.format(i),style='normal_text')
+  c_table.cell(0,0).add_paragraph(comment,style='normal_text')
 
 doc.save('report.docx')
