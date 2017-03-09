4df92ddc1cd9372f0a203523645928ff7784fe3c
Brady Lamprecht <bdlamprecht@gmail.com>
2017-03-09 16:08:24 -0700

    Close to final before beginning integration with 'todocxtpl.py'

--- docx_creator/quickstart/make_report.py
+++ docx_creator/quickstart/make_report.py
@@ -4,6 +4,10 @@ from docx import Document
 from docx.shared import Pt
 from docx.enum.style import WD_STYLE_TYPE
 
+# This was added to try and get the text inside tables to format
+# correctly by adding a paragraph immidiately before the table
+# and then remove it.  It never quite worked correctly as it
+# altered all tables in the resulting docx file.
 def delete_paragraph(paragraph):
   p = paragraph._element
   p.getparent().remove(p)
@@ -21,11 +25,11 @@ styles = doc.styles
 #styles['HTML Preformatted'].delete()
 
 paragraph_styles = [
-  s for s in styles if s.type == WD_STYLE_TYPE.PARAGRAPH
+  p for p in styles if p.type == WD_STYLE_TYPE.PARAGRAPH
 ]
 
 table_styles = [
-  s for s in styles if s.type == WD_STYLE_TYPE.TABLE
+  t for t in styles if t.type == WD_STYLE_TYPE.TABLE
 ]
 
 print('\nParapgrah Sytles:')
@@ -39,7 +43,10 @@ for style in table_styles:
 j2var_open  =  '{{ '
 j2var_close = ' }}'
 
-for i in range(1,3):
+#doc.add_paragraph('{{ test_name }} - {{ device }}', style='test_title')
+#h_table = doc.add_table(2,3)
+
+for i in range(1,4):
   doc.add_paragraph('Test {}'.format(i), style='test_header')
   doc.add_paragraph('Description', style='desc_header')
   doc.add_paragraph('This is test {} text.'.format(i), style='normal_text')
@@ -51,12 +58,12 @@ for i in range(1,3):
   doc.add_paragraph('[junos command here]', style='output')
 
   doc.add_paragraph('Test Output', style='desc_header')
-  result = j2var_open + 'test{}_result'.format(i) + j2var_close
+  result = j2var_open + 'test{}_result'.format(i) + j2var_close + '\n'
   o_table = doc.add_table(1,1,style='test_output')
   o_table.cell(0,0).add_paragraph(result,style='output_sm')
 
   doc.add_paragraph('Test Comments', style='desc_header')
-  comment = j2var_open + 'test{}_comment'.format(i) + j2var_close
+  comment = j2var_open + 'test{}_comment'.format(i) + j2var_close + '\n'
   c_table = doc.add_table(1,1,style='test_comment')
   c_table.cell(0,0).add_paragraph(comment,style='normal_text')
 
